0ff7a4e8d990ed5aafe08e8645be58ca1827832c
Alexey-aka-sultan <alex_240981@mail.ru>
2019-12-26 17:30:59 +0600

    fx

--- src/components/sectionFooter.vue
+++ src/components/sectionFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <div class="jumbotron jumbotron-fluid bg-dark text-white pl-5 pr-5 m-0 mt-5">
+    <div class="jumbotron jumbotron-fluid d-flex justify-content-center bg-dark text-white pl-5 pr-5 m-0 mt-5">
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas corporis praesentium dicta
         aliquid at. Reiciendis nesciunt fugiat maiores nisi, ut, possimus at doloribus amet eaque
